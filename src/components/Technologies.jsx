5acdcebe6573949163b8b3faac5a43a065f1f1f5
Ankitmaurya99 <153794491+Ankitmaurya99@users.noreply.github.com>
2024-09-05 23:47:05 +0530

    portfolio

---
+++ src/components/Technologies.jsx
@@ -0,0 +1,93 @@
+import React from "react";
+import { RiReactjsLine } from "react-icons/ri";
+import { FaNodeJs } from "react-icons/fa";
+import { SiMongodb } from "react-icons/si";
+import { RiTailwindCssFill } from "react-icons/ri";
+import { SiExpress } from "react-icons/si";
+import { SiPostman } from "react-icons/si";
+import { motion } from "framer-motion";
+
+const iconsVarients = (duration) => ({
+  initial: { y: -10 },
+  animate: {
+    y: [10, -10],
+    transition: {
+      duration: duration,
+      ease: "linear",
+      repeat: Infinity,
+      repeatType: "reverse",
+    },
+  },
+});
+
+const Technologies = () => {
+  return (
+    <div className="border-b border-neutral-800 pb-20">
+      <motion.h1
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 0.5 }}
+        className="my-20 text-center text-4xl"
+      >
+        Technologies
+      </motion.h1>
+      <motion.div
+        whileInView={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: -100 }}
+        transition={{ duration: 1.5 }}
+        className="flex flex-wrap item-center justify-center gap-4"
+      >
+        <motion.div
+          variants={iconsVarients(2.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <RiReactjsLine className="text-7xl text-cyan-400" />
+        </motion.div>
+        <motion.div
+          variants={iconsVarients(4.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <FaNodeJs className="text-7xl text-green-400" />
+        </motion.div>
+        <motion.div
+          variants={iconsVarients(5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <SiMongodb className="text-7xl text-green-500" />
+        </motion.div>
+        <motion.div
+          variants={iconsVarients(3)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <RiTailwindCssFill className="text-7xl text-blue-400" />
+        </motion.div>
+        <motion.div
+          variants={iconsVarients(6)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <SiExpress className="text-7xl text-green-400" />
+        </motion.div>
+        <motion.div
+          variants={iconsVarients(3.9)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <SiPostman className="text-7xl text-orange-400" />
+        </motion.div>
+      </motion.div>
+    </div>
+  );
+};
+
+export default Technologies;
